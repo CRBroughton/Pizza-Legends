bd46cdca7b9cee0b7708ee32422fc4819bc339a6
CRBroughton <crbroughton@posteo.uk>
2021-10-31 20:47:23 +0000

    rendered first character

--- src/Overworld.ts
+++ src/Overworld.ts
@@ -16,5 +16,22 @@ export default class Overworld {
             this.ctx.drawImage(image, 0, 0)
         }
         image.src = "images/maps/DemoLower.png";
+
+        const x = 5;
+        const y = 6;
+        const hero = new Image();
+        hero.onload = () => {
+            this.ctx.drawImage(
+                hero,
+                0, // left cut
+                0, // top cut
+                32,  // width of cut
+                32, // height of cut
+                x * 16 - 8, // cut compenstation
+                y * 16 - 18, 
+                32, // width and height of hero
+                32)
+        }
+        hero.src = "images/characters/people/hero.png"
     }
 }
