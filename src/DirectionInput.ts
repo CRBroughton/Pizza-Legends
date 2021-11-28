a40393040d5dc90a87e8eb7021e1995df1fbde26
CRBroughton <crbroughton@posteo.uk>
2021-11-28 15:15:57 +0000

    added keymap

--- src/DirectionInput.ts
+++ src/DirectionInput.ts
@@ -1,5 +1,18 @@
 export default class DirectionInput {
   constructor() {
     this.heldDirections = []
+
+    this.map = {
+      KeyW: 'up',
+      KeyA: 'left',
+      KeyS: 'down',
+      KeyD: 'right',
+    }
+  }
+
+  init() {
+    document.addEventListener('keydown', (e) => {
+      console.log(e.code)
+    })
   }
 }
