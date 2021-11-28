5f2541103ba70935e6760ea2cabd37bd650b053f
CRBroughton <crbroughton@posteo.uk>
2021-11-28 15:09:29 +0000

    fixed directionUpdate

--- src/Person.ts
+++ src/Person.ts
@@ -8,10 +8,10 @@ export default class Person extends GameObject {
     this.movingProgressRemaining = 0
 
     this.directionUpdate = {
-      up: ['y', 1],
+      up: ['y', -1],
       down: ['y', 1],
-      left: ['y', 1],
-      right: ['y', 1],
+      left: ['x', -1],
+      right: ['x', 1],
     }
   }
 
