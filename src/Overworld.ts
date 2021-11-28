fce2b9f46c74f581d79c1d651a65215b3e3b7163
CRBroughton <crbroughton@posteo.uk>
2021-11-28 12:33:39 +0000

    removed old code

--- src/Overworld.ts
+++ src/Overworld.ts
@@ -10,31 +10,16 @@ export default class Overworld {
     this.ctx = this.canvas.getContext('2d')!
   }
 
-  init() {
-    console.log('hello from the overworld', this)
-
-    const image = new Image()
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0)
+  startGameLoop() {
+    const step = () => {
+      requestAnimationFrame(() => {
+        step()
+      })
     }
-    image.src = 'images/maps/DemoLower.png'
+    step()
+  }
 
-    // Place game objects
-
-    const hero = new GameObject({
-      x: 5,
-      y: 6,
-    })
-
-    const npc1 = new GameObject({
-      x: 7,
-      y: 9,
-      src: 'images/characters/people/npc1.png',
-    })
-
-    setTimeout(() => {
-      hero.sprite.draw(this.ctx)
-      npc1.sprite.draw(this.ctx)
-    }, 200)
+  init() {
+    this.startGameLoop()
   }
 }
