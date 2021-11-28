224d9b1594ed8a3304104820bbafac545d2aee08
CRBroughton <crbroughton@posteo.uk>
2021-11-28 15:05:18 +0000

    added directions and updatePosition method

--- src/Person.ts
+++ src/Person.ts
@@ -2,8 +2,24 @@ import GameObject from './GameObject.js'
 
 export default class Person extends GameObject {
   movingProgressRemaining: number
-  constructor(config: { x: number; y: number; src?: string | undefined }) {
+
+  constructor(config) {
     super(config)
     this.movingProgressRemaining = 0
+
+    this.directionUpdate = {
+      up: ['y', 1],
+      down: ['y', 1],
+      left: ['y', 1],
+      right: ['y', 1],
+    }
+  }
+
+  updatePosition() {
+    if (this.movingProgressRemaining > 0) {
+      const [property, change] = this.directionUpdate[this.direction]
+      this[property] += change
+      this.movingProgressRemaining -= 1
+    }
   }
 }
