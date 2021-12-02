0295c4024ab59670cd179ef8eccdf17a5a31b6bf
CRBroughton <crbroughton@posteo.uk>
2021-12-02 21:18:43 +0000

    animations now have limits and progress

--- src/Sprite.ts
+++ src/Sprite.ts
@@ -37,11 +37,33 @@ export default class Sprite {
       'walk-up': [[1, 2], [0, 2], [3, 2], [0, 2]],
       'walk-left': [[1, 3], [0, 3], [3, 3], [0, 3]],
     }
-    this.currentAnimation = config.currentAnimation || 'idle-down'
+    this.currentAnimation = 'idle-right' // config.currentAnimation || 'idle-down'
     this.currentAnimationFrame = 0
+
+    this.animationFrameLimit = config.animationFrameLimit || 16
+    this.animationFrameProgress = this.animationFrameLimit
 
     // Reference the game object
     this.gameObject = config.gameObject
+  }
+
+  get frame() {
+    return this.animations[this.currentAnimation][this.currentAnimationFrame]
+  }
+
+  updateAnimationProgress() {
+    // downtick frame progress
+    if (this.animationFrameProgress > 0) {
+      this.animationFrameProgress -= 1
+      return
+    }
+
+    // reset the counter
+    this.animationFrameProgress = this.animationFrameLimit
+    this.currentAnimationFrame += 1
+
+    if (this.frame === undefined)
+      this.currentAnimationFrame = 0
   }
 
   draw(ctx: { drawImage: drawImage}) {
@@ -50,6 +72,10 @@ export default class Sprite {
 
     this.isShadowsLoaded && ctx.drawImage(this.shadow, x, y)
 
-    this.isLoaded && ctx.drawImage(this.image, 0, 0, 32, 32, x, y, 32, 32)
+    const [frameX, frameY] = this.frame
+
+    this.isLoaded && ctx.drawImage(this.image, frameX * 32, frameY * 32, 32, 32, x, y, 32, 32)
+
+    this.updateAnimationProgress()
   }
 }
