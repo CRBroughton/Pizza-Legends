9297ea86a63d9cc67bc5d9c003d1872e8d19b89c
CRBroughton <crbroughton@posteo.uk>
2021-12-03 20:45:40 +0000

    refactored utils.ts

---
+++ src/interfaces/OverworldMap.ts
@@ -0,0 +1,10 @@
+export interface cameraPerson {
+  x: number
+  y: number
+}
+
+export type takenSpace = (
+  x: number,
+  y: number,
+  direction: string,
+) => void
--- src/utils.ts
+++ src/utils.ts
@@ -1,27 +1,30 @@
+import { takenSpace } from '@/interfaces/OverworldMap'
+
 export const utils = {
   withGrid(n: number) {
     return n * 16
   },
-  asGridCoord(x, y) {
+  asGridCoord(x: number, y: number) {
     return `${x * 16},${y * 16}`
   },
-  nextPosition(initialX, initialY, direction) {
-    let x = initialX
-    let y = initialY
-    const size = 16
+}
 
-    if (direction === 'left')
-      x -= size
+export const nextPosition: takenSpace = (initialX, initialY, direction) => {
+  let x = initialX
+  let y = initialY
+  const size = 16
 
-    else if (direction === 'right')
-      x += size
+  if (direction === 'left')
+    x -= size
 
-    else if (direction === 'up')
-      y -= size
+  else if (direction === 'right')
+    x += size
 
-    else if (direction === 'down')
-      y += size
+  else if (direction === 'up')
+    y -= size
 
-    return { x, y }
-  },
+  else if (direction === 'down')
+    y += size
+
+  return { x, y }
 }
