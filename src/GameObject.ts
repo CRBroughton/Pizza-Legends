cadbd7d95315b868eedb43e366ecb7be9846c5d3
CRBroughton <crbroughton@posteo.uk>
2021-12-04 14:51:06 +0000

    added behaviourEvents to GameObject

--- src/GameObject.ts
+++ src/GameObject.ts
@@ -1,6 +1,7 @@
 import Sprite from '@/Sprite.js'
 import { Config, AddWall } from '@/types/GameObject'
 import { CameraPerson } from '@/types/OverworldMap'
+import OverworldEvent from '@/OverworldEvent.js'
 
 export default class GameObject {
   x: number
@@ -9,7 +10,9 @@ export default class GameObject {
   sprite: Sprite
   isMounted: boolean
   hero!: CameraPerson
+  id: null
   constructor(config: Config) {
+    this.id = null
     this.isMounted = false
     this.x = config.x || 0
     this.y = config.y || 0
@@ -18,10 +21,40 @@ export default class GameObject {
       gameObject: this,
       src: config.src || 'images/characters/people/hero.png',
     })
+
+    this.behaviourLoop = config.behaviourLoop || false
+    this.behaviourLoopIndex = 0
   }
 
   mount(map: { addWall: AddWall }) {
     this.isMounted = true
     map.addWall(this.x, this.y)
+
+    // If we have a behaviour, kick off after a short delay
+    setTimeout(() => {
+      this.doBehaviourEvent(map)
+    }, 10)
+  }
+
+  async doBehaviourEvent(map) {
+    // Dont do anythign if there is a more important cutscene or I don't have config to do anything
+    if (map.isCutscenePlaying || this.behaviourLoop.length === 0)
+      return
+
+    // Setting up our event with relevent info
+    const eventConfig = this.behaviourLoop[this.behaviourLoopIndex]
+    eventConfig.who = this.id
+
+    // Create an event instance out of our next event config
+    const eventHandler = new OverworldEvent({ map, event: eventConfig })
+    await eventHandler.init()
+
+    // Setting the next event to fire
+    this.behaviourLoopIndex++
+    if (this.behaviourLoopIndex === this.behaviourLoop.length)
+      this.behaviourLoopIndex = 0
+
+    // Do it again
+    this.doBehaviourEvent(map)
   }
 }
